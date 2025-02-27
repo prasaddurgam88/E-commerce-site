81006aa911f0010a30cded0138cd519042a96b11
prasaddurgam88 <prasaddurgam88@gmail.com>
2025-02-27 12:02:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+// const bar = document.getElementById('bar')
+// const nav = document.getElementById('navbar')
+
+
+// if(bar)
+//     {
+//         bar.addEventListener('click' , () =>
+//         {
+//             nav.classList.add('active');
+//         })
+//     }
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const navbarToggle = document.getElementById('navbarToggle');
+//     const navbarLinks = document.getElementById('navbar');
+
+//     navbarToggle.addEventListener('click', () => {
+//         navbarLinks.classList.toggle('active')
+//     });
+// });
+
+
+// <script>
+ 
+// const button = document.getElementById('myButton');
+// const nav = document.getElementById('navbar');
+
+
+
+// if(bar)
+//     {
+//         nav.addEventListener('click' , () =>
+//         {
+//             nav.classList.add('active');
+//         })
+//     }
+// </script>
+
+
+
+
+
+
+
+
+
